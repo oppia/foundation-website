f5acd5199e751b4dee429af2d64cc867ea22dd99
hoangviet1993 <hoangviet1993@gmail.com>
2018-07-19 11:48:16 -0400

    Moving navbar files into its own folder

---
+++ pages/navbar/NavigationBarDirective.js
@@ -0,0 +1,8 @@
+oppiaFoundationWebsite.directive('navigationBar', [function() {
+  return {
+    restrict: 'E',
+    scope: {},
+    templateUrl: '/pages/navbar/navbar.html',
+    controller: [function() {}]
+  };
+}]);
