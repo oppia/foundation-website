f6e427878604c7c0b317bd4fc343feb80cc84eba
hoangviet1993 <hoangviet1993@gmail.com>
2018-08-02 08:56:30 -0400

    Separating hard-coded volunteer info details into its own constant class

--- pages/volunteer/Volunteer.js
+++ pages/volunteer/Volunteer.js
@@ -13,87 +13,63 @@
 // limitations under the License.
 
 oppiaFoundationWebsite.controller('volunteerPage', [
-  '$scope', function($scope) {
-    var designSlideQuotes = [
-      'As a former teacher, I am motivated to create a platform that gives' +
-      'everyone equal access to education. Working with Oppia is a powerful' +
-      'opportunity to leverage design for social justice.',
-      'The public is more familiar with bad design than good design. It is, ' +
-      'in effect, conditioned to prefer bad design, because that is what it ' +
-      'lives with. The new becomes threatening, the old reassuring.',
-      'You can have an art experience in front of a Rembrandt… or in front ' +
-      'of a piece of graphic design.'
-    ];
+  '$scope', 'VOLUNTEER_INFO', function($scope, VOLUNTEER_INFO) {
+    var designSlideImages = [];
+    var designSlideQuotes = [];
+    var designSlideNames = [];
+    var designSlideJobTitles = [];
+    VOLUNTEER_INFO.design.forEach(function(designerProfile) {
+      designSlideImages.push(designerProfile.profilePictureImageFilename);
+      designSlideQuotes.push(designerProfile.professionalQuote);
+      designSlideNames.push(designerProfile.fullName);
+      designSlideJobTitles.push(designerProfile.jobTitle);
+    });
     $scope.designSlideQuotes = designSlideQuotes;
-    var designSlideNames = ['Jordan Movish', 'Milton Glaser', 'Paul Rand'];
     $scope.designSlideNames = designSlideNames;
-    var designSlideJobTitles = ['UX Design Lead', 'Graphic Designer',
-      'Art Director'];
     $scope.designSlideJobTitles = designSlideJobTitles;
-    var designTab = {
-      title: 'Design',
-      templateUrl: '/pages/volunteer/tabs_template/designTab.html'
-    };
-    var developmentSlideQuotes = [
-      'I’m excited to help build a platform that makes students feel ' +
-      'like they are learning from a personal tutor that acts as a guide and ' +
-      'can respond to their progress.',
-      'Measuring programming progress by lines of code is like measuring ' +
-      'aircraft building progress by weight.',
-      'It´s better to wait for a productive programmer to become available ' +
-      'than it is to wait for the first available programmer to become ' +
-      'productive.'
-    ];
+
+    var developmentSlideImages = [];
+    var developmentSlideQuotes = [];
+    var developmentSlideNames = [];
+    var developmentSlideJobTitles = [];
+    VOLUNTEER_INFO.development.forEach(function(developerProfile) {
+      developmentSlideImages.push(developerProfile.profilePictureImageFilename);
+      developmentSlideQuotes.push(developerProfile.professionalQuote);
+      developmentSlideNames.push(developerProfile.fullName);
+      developmentSlideJobTitles.push(developerProfile.jobTitle);
+    });
     $scope.developmentSlideQuotes = developmentSlideQuotes;
-    var developmentSlideNames = [
-      'Anmok Shukla', 'Bill Gates', 'Steve McConnell'];
     $scope.developmentSlideNames = developmentSlideNames;
-    var developmentSlideJobTitles = [
-      'Developer, Machine Learning', 'Former Microsoft CEO',
-      'Author of Code Complete'];
     $scope.developmentSlideJobTitles = developmentSlideJobTitles;
-    var developmentTab = {
-      title: 'Development',
-      templateUrl: '/pages/volunteer/tabs_template/developmentTab.html'
-    };
-    var researchSlideQuotes = [
-      'I love that Oppia is striving to make learning and education more fun ' +
-      'and accessible to all. Volunteering gives me a chance to gain exp as a' +
-      ' user researcher while giving back to a cause that I value.',
-      'When UX doesn’t consider ALL users, shouldn’t it be known as “SOME ' +
-      'User Experience” or… SUX?',
-      'The next big thing is the one that makes the last big thing usable.'
-    ];
+
+    var researchSlideImages = [];
+    var researchSlideQuotes = [];
+    var researchSlideNames = [];
+    var researchSlideJobTitles = [];
+    VOLUNTEER_INFO.research.forEach(function(researcherProfile) {
+      researchSlideImages.push(researcherProfile.profilePictureImageFilename);
+      researchSlideQuotes.push(researcherProfile.professionalQuote);
+      researchSlideNames.push(researcherProfile.fullName);
+      researchSlideJobTitles.push(researcherProfile.jobTitle);
+    });
     $scope.researchSlideQuotes = researchSlideQuotes;
-    var researchSlideNames = ['Rachen Chan', 'Billy Gregory', 'Blake Ross'];
     $scope.researchSlideNames = researchSlideNames;
-    var researchSlideJobTitles = ['User Researcher Lead',
-      'Senior Accessibility Engineer', 'Co-founder of Mozilla Firefox'];
     $scope.researchSlideJobTitles = researchSlideJobTitles;
-    var researchTab = {
-      title: 'Research',
-      templateUrl: '/pages/volunteer/tabs_template/researchTab.html'
-    };
-    var marketingSlideQuotes = [
-      'I worked for an education software company for 13 years and saw the ' +
-      'tremendous impact it can have on students. My work at Oppia helps me ' +
-      'continue shaping the future of education technology.',
-      'You need the kind of objectivity that makes you forget everything ' +
-      'you’ve heard, clear the table, and do a factual study like a scientist' +
-      ' would.',
-      'Good content isn’t about good storytelling. It’s about telling a true ' +
-      'story well.'
-    ];
+
+    var marketingSlideImages = [];
+    var marketingSlideQuotes = [];
+    var marketingSlideNames = [];
+    var marketingSlideJobTitles = [];
+    VOLUNTEER_INFO.marketing.forEach(function(marketerProfile) {
+      marketingSlideImages.push(marketerProfile.profilePictureImageFilename);
+      marketingSlideQuotes.push(marketerProfile.professionalQuote);
+      marketingSlideNames.push(marketerProfile.fullName);
+      marketingSlideJobTitles.push(marketerProfile.jobTitle);
+    });
     $scope.marketingSlideQuotes = marketingSlideQuotes;
-    var marketingSlideNames = ['Joe Lastname', 'Steve Wozniak', 'Ann Handley'];
     $scope.marketingSlideNames = marketingSlideNames;
-    var marketingSlideJobTitles = ['Joe\'s Job', 'Co-founder of Apple',
-      'Digital Marketing Pioneer, Writer, Speaker'];
     $scope.marketingSlideJobTitles = marketingSlideJobTitles;
-    var marketingTab = {
-      title: 'Marketing',
-      templateUrl: '/pages/volunteer/tabs_template/marketingTab.html'
-    };
+
     var createSlide = function(profileSlides, profileImage, slideId) {
       profileSlides.push({
         image: profileImage,
@@ -115,6 +91,23 @@ oppiaFoundationWebsite.controller('volunteerPage', [
       createSlide($scope.researchSlides, imageUrl, i);
       createSlide($scope.marketingSlides, imageUrl, i);
     }
+
+    var designTab = {
+      title: 'Design',
+      templateUrl: '/pages/volunteer/tabs_template/designTab.html'
+    };
+    var developmentTab = {
+      title: 'Development',
+      templateUrl: '/pages/volunteer/tabs_template/developmentTab.html'
+    };
+    var researchTab = {
+      title: 'Research',
+      templateUrl: '/pages/volunteer/tabs_template/researchTab.html'
+    };
+    var marketingTab = {
+      title: 'Marketing',
+      templateUrl: '/pages/volunteer/tabs_template/marketingTab.html'
+    };
     var tabs = [designTab, developmentTab, researchTab, marketingTab];
     $scope.tabs = tabs;
     $scope.tabId = 0;
---
+++ pages/volunteer/VolunteerInfoDefinitions.js
@@ -0,0 +1,101 @@
+// Copyright 2018 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+oppiaFoundationWebsite.constant('VOLUNTEER_INFO', {
+  design: [{
+    fullName: 'Jordan Movish',
+    profilePictureImageFilename: '',
+    professionalQuote: 'As a former teacher, I am motivated to create a ' +
+    'platform that gives everyone equal access to education. Working with ' +
+    'Oppia is a powerful opportunity to leverage design for social justice.',
+    jobTitle: 'UX Design Lead'
+  }, {
+    fullName: 'Milton Glaser',
+    profilePictureImageFilename: '',
+    professionalQuote: 'The public is more familiar with bad design than ' +
+    'good design. It is, in effect, conditioned to prefer bad design, ' +
+    'because that is what it lives with. The new becomes threatening, the ' +
+    'old reassuring.',
+    jobTitle: 'Graphic Designer'
+  }, {
+    fullName: 'Paul Rand',
+    profilePictureImageFilename: '',
+    professionalQuote: 'You can have an art experience in front of a ' +
+    'Rembrandt… or in front of a piece of graphic design.',
+    jobTitle: 'Art Director'
+  }],
+  development: [{
+    fullName: 'Anmol Shukla',
+    profilePictureImageFilename: '',
+    professionalQuote: 'I’m excited to help build a platform that makes ' +
+    'students feel like they are learning from a personal tutor that acts as ' +
+    'a guide and can respond to their progress.',
+    jobTitle: 'Developer, Machine Learning'
+  }, {
+    fullName: 'Bill Gates',
+    profilePictureImageFilename: '',
+    professionalQuote: 'Measuring programming progress by lines of code is ' +
+    'like measuring aircraft building progress by weight.',
+    jobTitle: 'Co-Founder and Technology Advisor of Microsoft'
+  }, {
+    fullName: 'Steve McConnell',
+    profilePictureImageFilename: '',
+    professionalQuote: 'It´s better to wait for a productive programmer to ' +
+    'become available than it is to wait for the first available programmer ' +
+    'to become productive.',
+    jobTitle: 'Author of Code Complete'
+  }],
+  research: [{
+    fullName: 'Rachen Chan',
+    profilePictureImageFilename: '',
+    professionalQuote: 'I love that Oppia is striving to make learning and ' +
+    'education more fun and accessible to all. Volunteering gives me a ' +
+    'chance to gain exp as a user researcher while giving back to a cause ' +
+    'that I value.',
+    jobTitle: 'User Researcher Lead'
+  }, {
+    fullName: 'Billy Gregory',
+    profilePictureImageFilename: '',
+    professionalQuote: 'When UX doesn’t consider ALL users, shouldn’t it be ' +
+    'known as “SOME User Experience” or… SUX?',
+    jobTitle: 'Senior Accessibility Engineer'
+  }, {
+    fullName: 'Blake Ross',
+    profilePictureImageFilename: '',
+    professionalQuote: 'The next big thing is the one that makes the last ' +
+    'big thing usable.',
+    jobTitle: 'Co-Founder of Mozilla Firefox'
+  }],
+  marketing: [{
+    fullName: 'Joe Yannaphol',
+    profilePictureImageFilename: '',
+    professionalQuote: 'I worked for an education software company for 13 ' +
+    'years and saw the tremendous impact it can have on students. My work at ' +
+    'Oppia helps me continue shaping the future of education technology.',
+    jobTitle: 'Marketing Lead'
+  }, {
+    fullName: 'Steve Wozniak',
+    profilePictureImageFilename: '',
+    professionalQuote: 'You need the kind of objectivity that makes you ' +
+    'forget everything you’ve heard, clear the table, and do a factual study ' +
+    'like a scientist would.',
+    jobTitle: 'Co-Founder of Apple'
+  }, {
+    fullName: 'Ann Handley',
+    profilePictureImageFilename: '',
+    professionalQuote: 'Good content isn’t about good storytelling. It’s ' +
+    'about telling a true story well.',
+    jobTitle: 'Digital Marketing Pioneer, Writer, Speaker'
+  }]
+});
