c18ebee612ea4bce74e5418483e6384d51ce5da2
hoangviet1993 <hoangviet1993@gmail.com>
2018-08-04 00:08:43 -0400

    Defining scope.tabs directly, renaming variables for clarity and removing duplicated slide variables

--- pages/volunteer/Volunteer.js
+++ pages/volunteer/Volunteer.js
@@ -12,75 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-oppiaFoundationWebsite.controller('volunteerPage', [
-  '$scope', function(
-      $scope) {
-    var designTab = {
-      title: 'Design',
-      templateUrl: '/pages/volunteer/tabs_template/designTab.html'
-    };
-    var developmentTab = {
-      title: 'Development',
-      templateUrl: '/pages/volunteer/tabs_template/developmentTab.html'
-    };
-    var researchTab = {
-      title: 'Research',
-      templateUrl: '/pages/volunteer/tabs_template/researchTab.html'
-    };
-    var marketingTab = {
-      title: 'Marketing',
-      templateUrl: '/pages/volunteer/tabs_template/marketingTab.html'
-    };
-    var tabs = [designTab, developmentTab, researchTab, marketingTab];
-    $scope.tabs = tabs;
-    $scope.tabId = 0;
-    $scope.templateUrl = tabs[0].templateUrl;
-    $scope.onTabSelected = function(tabId) {
-      $scope.tabId = tabId;
-      $scope.templateUrl = tabs[tabId].templateUrl;
-    };
-    $scope.slideInterval = 0;
-    $scope.noWrapSlides = false;
-  }]);
-
-oppiaFoundationWebsite.factory('VolunteerProfilesService', [
-  'VOLUNTEER_INFO', function(VOLUNTEER_INFO) {
-    /**
-     * Select 3 unique random volunteer profiles.
-     * @param {Array} volunteerProfiles - Profiles of all volunteers.
-     */
-    var getRandomProfiles = function(volunteerProfiles) {
-      // Cloning volunteerProfiles to prevent changes to the original profiles.
-      var volunteerProfiles = Object.assign([], volunteerProfiles);
-      var slides = [];
-      for (var i = volunteerProfiles.length - 1; i >= 0; i--) {
-        var randomIndex = Math.floor(Math.random() * volunteerProfiles.length);
-        slides.push(volunteerProfiles[randomIndex]);
-        if (slides.length === 3) {
-          // Only adding 3 profiles currently.
-          return slides;
-        }
-        volunteerProfiles.splice(randomIndex, 1);
-      }
-    };
-    var designProfiles = getRandomProfiles(VOLUNTEER_INFO.design);
-    var developmentProfiles = getRandomProfiles(VOLUNTEER_INFO.development);
-    var researchProfiles = getRandomProfiles(VOLUNTEER_INFO.research);
-    var marketingProfiles = getRandomProfiles(VOLUNTEER_INFO.marketing);
-
-    var VolunteerProfilesService = {
-      getDesignProfiles: function() {
-        return designProfiles;
-      },
-      getDevelopmentProfiles: function() {
-        return developmentProfiles;
-      },
-      getResearchProfiles: function() {
-        return researchProfiles;
-      },
-      getMarketingProfiles: function() {
-        return marketingProfiles;
-      }
-    };
-    return VolunteerProfilesService;
-  }]);
+oppiaFoundationWebsite.controller('VolunteerPage', ['$scope', function($scope) {
+  $scope.tabs = [{
+    title: 'Design',
+    templateUrl: '/pages/volunteer/tabs_template/design_tab.html'
+  }, {
+    title: 'Development',
+    templateUrl: '/pages/volunteer/tabs_template/development_tab.html'
+  }, {
+    title: 'Research',
+    templateUrl: '/pages/volunteer/tabs_template/research_tab.html'
+  }, {
+    title: 'Marketing',
+    templateUrl: '/pages/volunteer/tabs_template/marketing_tab.html'
+  }];
+  $scope.activeTabId = 0;
+  $scope.templateUrl = $scope.tabs[0].templateUrl;
+  $scope.onTabSelected = function(activeTabId) {
+    $scope.activeTabId = activeTabId;
+  };
+}]);
