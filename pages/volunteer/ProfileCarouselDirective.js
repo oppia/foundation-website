5ca7911d0b0f33886d7f18370368edcfad5ceff6
hoangviet1993 <hoangviet1993@gmail.com>
2018-08-04 08:43:23 -0400

    Rename variables, add error throwing and catching and update markup files

--- pages/volunteer/ProfileCarouselDirective.js
+++ pages/volunteer/ProfileCarouselDirective.js
@@ -14,10 +14,6 @@
 
 oppiaFoundationWebsite.constant('CAROUSEL_TYPE_DESKTOP', 'desktop');
 oppiaFoundationWebsite.constant('CAROUSEL_TYPE_MOBILE', 'mobile');
-oppiaFoundationWebsite.constant('DESIGN_TAB', 'design');
-oppiaFoundationWebsite.constant('DEVELOPMENT_TAB', 'development');
-oppiaFoundationWebsite.constant('RESEARCH_TAB', 'research');
-oppiaFoundationWebsite.constant('MARKETING_TAB', 'marketing');
 
 oppiaFoundationWebsite.directive('profileCarousel', [function() {
   return {
@@ -28,41 +24,34 @@ oppiaFoundationWebsite.directive('profileCarousel', [function() {
     },
     templateUrl: '/pages/volunteer/profile_carousel_directive.html',
     controller: [
-      '$scope', 'VolunteerProfilesService', 'CAROUSEL_TYPE_DESKTOP',
-      'CAROUSEL_TYPE_MOBILE', 'DESIGN_TAB', 'DEVELOPMENT_TAB', 'RESEARCH_TAB',
-      'MARKETING_TAB',
+      '$scope', '$log', 'VolunteerProfilesService', 'CAROUSEL_TYPE_DESKTOP',
+      'CAROUSEL_TYPE_MOBILE',
       function(
-          $scope, VolunteerProfilesService, CAROUSEL_TYPE_DESKTOP,
-          CAROUSEL_TYPE_MOBILE, DESIGN_TAB, DEVELOPMENT_TAB, RESEARCH_TAB,
-          MARKETING_TAB) {
+          $scope, $log, VolunteerProfilesService, CAROUSEL_TYPE_DESKTOP,
+          CAROUSEL_TYPE_MOBILE) {
         if ($scope.carouselType === CAROUSEL_TYPE_DESKTOP) {
           $scope.displayDesktopCarousel = true;
           $scope.displayMobileCarousel = false;
-        }
-        if ($scope.carouselType === CAROUSEL_TYPE_MOBILE) {
+        } else if ($scope.carouselType === CAROUSEL_TYPE_MOBILE) {
           $scope.displayDesktopCarousel = false;
           $scope.displayMobileCarousel = true;
+        } else {
+          try {
+            throw Error('Incorrect carousel type used in markup file.');
+          } catch (error) {
+            $log.error(error);
+          }
         }
         // Please visit https://angular-ui.github.io/bootstrap/#!#carousel
         // for more info on available directives.
         // Time interval to cycle through the slides. 0 for no auto-cycling.
-        $scope.slideIntervalInMS = 0;
-        // Prevent looping of slides.
+        $scope.slideIntervalInMillisecs = 0;
+        // Allow looping of slides.
         $scope.noWrapSlides = false;
         // Index of current active slide.
-        $scope.active = 0;
-        if ($scope.correspondingTabName === DESIGN_TAB) {
-          $scope.slides = VolunteerProfilesService.getDesignProfiles();
-        }
-        if ($scope.correspondingTabName === DEVELOPMENT_TAB) {
-          $scope.slides = VolunteerProfilesService.getDevelopmentProfiles();
-        }
-        if ($scope.correspondingTabName === RESEARCH_TAB) {
-          $scope.slides = VolunteerProfilesService.getResearchProfiles();
-        }
-        if ($scope.correspondingTabName === MARKETING_TAB) {
-          $scope.slides = VolunteerProfilesService.getMarketingProfiles();
-        }
+        $scope.activeSlideIndex = 0;
+        $scope.slides = VolunteerProfilesService.getSlidesForTab(
+          $scope.correspondingTabName);
       }]
   };
 }]);
