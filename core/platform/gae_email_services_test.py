9601dc9c181f7a0400b8b0d1d5345c105321b6e0
hoangviet1993 <hoangviet1993@gmail.com>
2018-08-29 11:39:24 -0400

    Stubbing out test classes

---
+++ core/platform/gae_email_services_test.py
@@ -0,0 +1,24 @@
+# Copyright 2018 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the GAE mail API wrapper."""
+
+from core.tests import app_engine_test_base
+
+
+class EmailTests(app_engine_test_base.GenericTestBase):
+    """Tests for sending emails."""
+
+    def test_send_mail(self):
+        pass
