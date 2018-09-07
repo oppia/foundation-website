7e1926943e70fe50c7c1f68515e0f8fef63e2f23
hoangviet1993 <hoangviet1993@gmail.com>
2018-09-07 09:21:22 -0400

    Fix file docstring

--- core/utility/constants_test.py
+++ core/utility/constants_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for Constants object and cosntants.json file."""
+"""Tests for CONSTANTS object and cosntants.json file."""
 
 import os
 
