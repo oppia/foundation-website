44633f441851f95edaeb16ce676b8bd093d11006
hoangviet1993 <hoangviet1993@gmail.com>
2018-10-10 15:56:08 -0400

    Completely remove any references to logger_test

--- app/core/tests/unit_test_base.py
+++ app/core/tests/unit_test_base.py
@@ -16,8 +16,6 @@
 
 import unittest
 
-from core.tests.logger_test import _AssertLogsContext
-
 
 class TestBase(unittest.TestCase):
     """Base class for all tests."""
@@ -36,26 +34,3 @@ class TestBase(unittest.TestCase):
         Subclasses of TestBase should override this method.
         """
         raise NotImplementedError
-
-
-    def assertLogs(self, logger=None, level=None):
-        """Fail unless a log message of level *level* or higher is emitted
-        on *logger_name* or its children.  If omitted, *level* defaults to
-        INFO and *logger* defaults to the root logger.
-
-        This method must be used as a context manager, and will yield
-        a recording object with two attributes: `output` and `records`.
-        At the end of the context manager, the `output` attribute will
-        be a list of the matching formatted log messages and the
-        `records` attribute will be a list of the corresponding LogRecord
-        objects.
-
-        Example::
-
-            with self.assertLogs('foo', level='INFO') as cm:
-                logging.getLogger('foo').info('first message')
-                logging.getLogger('foo.bar').error('second message')
-            self.assertEqual(cm.output, ['INFO:foo:first message',
-                                         'ERROR:foo.bar:second message'])
-        """
-        return _AssertLogsContext(self, logger, level)
