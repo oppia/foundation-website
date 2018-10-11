155ed78bda5c4381dbdad2ada7b295b6fb631945
hoangviet1993 <hoangviet1993@gmail.com>
2018-10-11 07:58:01 -0400

    Remove extra -v flag and revert changes to gae_suite

--- app/core/tests/gae_suite.py
+++ app/core/tests/gae_suite.py
@@ -35,7 +35,7 @@ CORE_DIR = os.path.join(CURR_DIR, 'app', '')
 OPPIA_TOOLS_DIR = os.path.join(CURR_DIR, '..', 'oppia_tools')
 
 DIRS_TO_ADD_TO_SYS_PATH = [
-    CORE_DIR,
+    CURR_DIR,
     os.path.join(
         OPPIA_TOOLS_DIR, 'google_appengine_1.9.73', 'google_appengine'),
     os.path.join(
