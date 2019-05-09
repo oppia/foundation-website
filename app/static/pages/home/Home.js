233c8dde8df56a1f4177e125bca7d4ba6280328f
Tran Quoc Hoang Viet <hoangviet1993@gmail.com>
2019-05-08 22:56:29 -0700

    Use scope var to serve static string for home page

--- app/static/pages/home/Home.js
+++ app/static/pages/home/Home.js
@@ -1,4 +1,4 @@
-// Copyright 2018 The Oppia Authors. All Rights Reserved.
+// Copyright 2019 The Oppia Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,9 +13,36 @@
 // limitations under the License.
 
 oppiaFoundationWebsite.controller('HomePage', [
-  'WindowDimensionsService', '$scope', function(
-      WindowDimensionsService, $scope) {
+  'LearnerProfilesService', 'WindowDimensionsService', '$scope', function(
+      LearnerProfilesService, WindowDimensionsService, $scope) {
     $scope.desktopView = WindowDimensionsService.isDesktopViewWidth();
+    $scope.studentProblemsTextContent = [{
+      heading: '1. Discrimination',
+      text: 'Up to 95% of children with disabilities are out of school. At ' +
+      'least 1 in 5 adolescent girls globally are denied an education.'
+    }, {
+      heading: '2. Lack of resources',
+      text: 'In 1 out of 3 countries, less than 3/4 of teachers are trained ' +
+      'to national standards. Millions lack textbooks and related resources.'
+    }, {
+      heading: '3. Conflict',
+      text: 'In 2017, 27 million of the 50 million children living in ' +
+      'countries affected by conflicts were out of school.'
+    }];
+    $scope.impactTextContent = [{
+      number: '1,000,000+',
+      text: 'Learners served worldwide'
+    }, {
+      number: '20,000',
+      text: 'Lessons in our virtual library'
+    }, {
+      number: '1',
+      text: 'Randomized trial completed. More to come!'
+    }, {
+      number: '100',
+      text: 'Volunteers from all over the glove'
+    }];
+    $scope.learnerProfiles = LearnerProfilesService.getLearnerProfiles();
     WindowDimensionsService.registerOnResizeHook(function() {
       $scope.desktopView = WindowDimensionsService.isDesktopViewWidth();
       $scope.$apply();
